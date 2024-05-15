9ef252ec83bb5a04e3eed3137661098c5d8d42c1
loganrains <logankrains@gmail.com>
2024-05-15 15:11:22 -0500

    Created MenuItem Class, Started Menu Class

--- RESTAURANT-MENU/Program.cs
+++ RESTAURANT-MENU/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.CompilerServices;
+
+public class MenuItem
+{
+    private float price;
+    private string description;
+    private string category;
+    private bool isNew;
+
+
+    public float Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public string Category
+    {
+        get { return category; }
+        set { description = value; }
+    }
+
+    public bool IsNew
+    {
+        get { return isNew; }
+        set { isNew = value; }
+    }
+
+
+    public MenuItem(float price, string description, string category, bool isNew)
+    {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+
+}
+
+public class Menu
+{
+    private List<MenuItem> items;
+    
+    
+    private static void AddItem(MenuItem)
+    {
+        Menu.item.Add(MenuItem);
+    }
+    
+}
